6d9b7cfffe5253cc19559a0d40dbb1ebac461a14
jrfilocao <>
2019-08-22 18:53:02 +0200

    Write out words in bucc.sh

--- tasks/bucc/bucc.sh
+++ tasks/bucc/bucc.sh
@@ -21,18 +21,18 @@ if [ -z ${LASER+x} ] ; then
 fi
 
 # general config
-bucc="bucc2018"
-data="."
-xdir=${data}/downloaded	# tar files as distrubuted by the BUCC evaluation
-ddir=${data}/${bucc}	# raw texts of BUCC
-edir=${data}/embed	# normalized texts and embeddings
-langs=("fr" "de" "ru" "zh")
-ltrg="en"		# English is always the 2nd language
+bucc_edition="bucc2018"
+root_directory="."
+bucc_tar_files_directory=${root_directory}/downloaded # tar files as distrubuted by the BUCC evaluation
+bucc_raw_texts_directory=${root_directory}/${bucc_edition} # raw texts of BUCC
+normalized_texts_embeddings_directory=${root_directory}/embed # normalized texts and embeddings
+languages=("fr" "de" "ru" "zh")
+target_language="en" # English is always the 2nd language
 
 # encoder
-model_dir="${LASER}/models"
-encoder="${model_dir}/bilstm.93langs.2018-12-26.pt"
-bpe_codes="${model_dir}/93langs.fcodes"
+models_directory="${LASER}/models"
+encoder="${models_directory}/bilstm.93langs.2018-12-26.pt"
+bpe_codes="${models_directory}/93langs.fcodes"
 
 
 ###################################################################
@@ -42,33 +42,32 @@ bpe_codes="${model_dir}/93langs.fcodes"
 ###################################################################
 
 GetData () {
-  fn1=$1; fn2=$2; lang=$3
-  outf="${edir}/${bucc}.${lang}-${ltrg}.${fn2}"
-  for ll  in ${ltrg} ${lang} ; do
-    inf="${ddir}/${fn1}.${ll}"
-    if [ ! -f ${outf}.txt.${ll} ] ; then
-      echo " - extract files ${outf} in ${ll}"
-      cat ${inf} | cut -f1 > ${outf}.id.${ll}
-      cat ${inf} | cut -f2 > ${outf}.txt.${ll}
+  dataset_path=$1; dataset_type=$2; source_language=$3
+  file_output="${normalized_texts_embeddings_directory}/${bucc_edition}.${source_language}-${target_language}.${dataset_type}"
+  for language in ${target_language} ${source_language} ; do
+    dataset_path_for_language="${bucc_raw_texts_directory}/${dataset_path}.${language}"
+    if [ ! -f ${file_output}.txt.${language} ] ; then
+      echo " - extract files ${file_output} in ${language}"
+      cat ${dataset_path_for_language} | cut -f1 > ${file_output}.id.${language}
+      cat ${dataset_path_for_language} | cut -f2 > ${file_output}.txt.${language}
     fi
   done
 }
 
 ExtractBUCC () {
-  slang=$1
-  tlang=${ltrg}
+  source_language=$1
 
-  pushd ${data} > /dev/null
-  if [ ! -d ${ddir}/${slang}-${tlang} ] ; then
-    for tf in ${xdir}/${bucc}-${slang}-${tlang}.*.tar.bz2 ; do
-      echo " - extract from tar `basename ${tf}`"
-      tar jxf $tf
+  pushd ${root_directory} > /dev/null
+  if [ ! -d ${bucc_raw_texts_directory}/${source_language}-${target_language} ] ; then
+    for tar_file in ${bucc_tar_files_directory}/${bucc_edition}-${source_language}-${target_language}.*.tar.bz2 ; do
+      echo " - extract from tar `basename ${tar_file}`"
+      tar jxf $tar_file
     done
   fi
 
-  GetData "${slang}-${tlang}/${slang}-${tlang}.sample" "dev" ${slang}
-  GetData "${slang}-${tlang}/${slang}-${tlang}.training" "train" ${slang}
-  GetData "${slang}-${tlang}/${slang}-${tlang}.test" "test" ${slang}
+  GetData "${source_language}-${target_language}/${source_language}-${target_language}.sample" "dev" ${source_language}
+  GetData "${source_language}-${target_language}/${source_language}-${target_language}.training" "train" ${source_language}
+  GetData "${source_language}-${target_language}/${source_language}-${target_language}.test" "test" ${source_language}
   popd > /dev/null
 }
 
@@ -80,16 +79,16 @@ ExtractBUCC () {
 ###################################################################
 
 Embed () {
-  ll=$2
-  txt="$1.txt.${ll}"
-  enc="$1.enc.${ll}"
-  if [ ! -s ${enc} ] ; then
-    cat ${txt} | python3 ${LASER}/source/embed.py \
+  language=$2
+  text_for_language="$1.txt.${language}"
+  sentence_embedding_output_path_for_language="$1.enc.${language}"
+  if [ ! -s ${sentence_embedding_output_path_for_language} ] ; then
+    cat ${text_for_language} | python3 ${LASER}/source/embed.py \
       --encoder ${encoder} \
-      --token-lang ${ll} \
+      --token-lang ${language} \
       --bpe-codes ${bpe_codes} \
-      --output ${enc} \
-      --verbose 
+      --output ${sentence_embedding_output_path_for_language} \
+      --verbose
   fi
 }
 
@@ -101,17 +100,17 @@ Embed () {
 ###################################################################
 
 Mine () {
-  bn=$1
-  l1=$2
-  l2=$3
-  cand="${bn}.candidates.tsv"
-  if [ ! -s ${cand} ] ; then
+  dataset_path=$1
+  first_language=$2
+  second_language=$3
+  candidates="${dataset_path}.candidates.tsv"
+  if [ ! -s ${candidates} ] ; then
     python3 ${LASER}/source/mine_bitexts.py \
-       ${bn}.txt.${l1} ${bn}.txt.${l2} \
-       --src-lang ${l1} --trg-lang ${l2} \
-       --src-embeddings ${bn}.enc.${l1} --trg-embeddings ${bn}.enc.${l2} \
+       ${dataset_path}.txt.${first_language} ${dataset_path}.txt.${second_language} \
+       --src-lang ${first_language} --trg-lang ${second_language} \
+       --src-embeddings ${dataset_path}.enc.${first_language} --trg-embeddings ${dataset_path}.enc.${second_language} \
        --unify --mode mine --retrieval max --margin ratio -k 4  \
-       --output ${cand} \
+       --output ${candidates} \
        --verbose --gpu
   fi
 }
@@ -123,55 +122,55 @@ Mine () {
 #
 ###################################################################
 
-echo -e "\nProcessing BUCC data in ${data}"
+echo -e "\nProcessing BUCC data in ${root_directory}"
 
 # create output directories
-for d in ${ddir} ${edir} ; do
-  mkdir -p ${d}
+for directory in ${bucc_raw_texts_directory} ${normalized_texts_embeddings_directory} ; do
+  mkdir -p ${directory}
 done
 
-for lsrc in ${langs[@]} ; do
-  ExtractBUCC ${lsrc}
+for source_language in ${languages[@]} ; do
+  ExtractBUCC ${source_language}
 
-  # Tokenize and embed train 
-  bname="${bucc}.${lsrc}-${ltrg}"
-  part="${bname}.train"
-  Embed ${edir}/${part} ${lsrc} ${encoder} ${bpe_codes}
-  Embed ${edir}/${part} ${ltrg} ${encoder} ${bpe_codes}
+  # Tokenize and embed train
+  base_file_name="${bucc_edition}.${source_language}-${target_language}"
+  dataset_path="${base_file_name}.train"
+  Embed ${normalized_texts_embeddings_directory}/${dataset_path} ${source_language} ${encoder} ${bpe_codes}
+  Embed ${normalized_texts_embeddings_directory}/${dataset_path} ${target_language} ${encoder} ${bpe_codes}
 
   # mine for texts in train
-  Mine ${edir}/${part} ${lsrc} ${ltrg}
+  Mine ${normalized_texts_embeddings_directory}/${dataset_path} ${source_language} ${target_language}
 
   # optimize threshold on BUCC training data and provided gold alignments
-  if [ ! -s ${part}.log ] ; then
+  if [ ! -s ${dataset_path}.log ] ; then
     python3 bucc.py \
-      --src-lang ${lsrc} --trg-lang ${ltrg} \
-      --bucc-texts ${edir}/${part}.txt \
-      --bucc-ids ${edir}/${part}.id \
-      --candidates ${edir}/${part}.candidates.tsv \
-      --gold ${ddir}/${lsrc}-${ltrg}/${lsrc}-${ltrg}.training.gold \
+      --src-lang ${source_language} --trg-lang ${target_language} \
+      --bucc-texts ${normalized_texts_embeddings_directory}/${dataset_path}.txt \
+      --bucc-ids ${normalized_texts_embeddings_directory}/${dataset_path}.id \
+      --candidates ${normalized_texts_embeddings_directory}/${dataset_path}.candidates.tsv \
+      --gold ${bucc_raw_texts_directory}/${source_language}-${target_language}/${source_language}-${target_language}.training.gold \
       --verbose \
-      | tee ${part}.log
+      | tee ${dataset_path}.log
   fi
 
-  # Tokenize and embed test 
-  part="${bname}.test"
-  Embed ${edir}/${part} ${lsrc} ${encoder} ${bpe_codes}
-  Embed ${edir}/${part} ${ltrg} ${encoder} ${bpe_codes}
+  # Tokenize and embed test
+  dataset_path="${base_file_name}.test"
+  Embed ${normalized_texts_embeddings_directory}/${dataset_path} ${source_language} ${encoder} ${bpe_codes}
+  Embed ${normalized_texts_embeddings_directory}/${dataset_path} ${target_language} ${encoder} ${bpe_codes}
 
   # mine for texts in test
-  Mine ${edir}/${part} ${lsrc} ${ltrg}
+  Mine ${normalized_texts_embeddings_directory}/${dataset_path} ${source_language} ${target_language}
 
   # extract test bitexts for treshhold optimized on train
-  th=`grep 'best threshold' ${bname}.train.log | sed -e 's/[=:]/ /g' | awk '{print $4}'`
-  extracted="${edir}/${part}.extracted.tsv"
+  threshold=`grep 'best threshold' ${base_file_name}.train.log | sed -e 's/[=:]/ /g' | awk '{print $4}'`
+  extracted="${normalized_texts_embeddings_directory}/${dataset_path}.extracted.tsv"
   if [ ! -s ${extracted} ] ; then
     python3 bucc.py \
-      --src-lang ${lsrc} --trg-lang ${ltrg} \
-      --bucc-texts ${edir}/${part}.txt \
-      --bucc-ids ${edir}/${part}.id \
-      --candidates ${edir}/${part}.candidates.tsv \
-      --threshold ${th} --output ${extracted} \
+      --src-lang ${source_language} --trg-lang ${target_language} \
+      --bucc-texts ${normalized_texts_embeddings_directory}/${dataset_path}.txt \
+      --bucc-ids ${normalized_texts_embeddings_directory}/${dataset_path}.id \
+      --candidates ${normalized_texts_embeddings_directory}/${dataset_path}.candidates.tsv \
+      --threshold ${threshold} --output ${extracted} \
       --verbose
   fi
 done
@@ -180,21 +179,21 @@ done
 # using a (conservative) threshold of 1.1
 # All the data is supposed to be already tokenized
 
-th=1.1
-for lsrc in ${langs[@]} ; do
-  for ltrg in ${langs[@]} ; do
-    if [ ${lsrc} != 'en' -a ${ltrg} != "en" -a ${lsrc} != ${ltrg} ] ; then
-      bitext="${bucc}.${lsrc}-${ltrg}.train.extracted.th${th}.csv"
+threshold=1.1
+for source_language in ${languages[@]} ; do
+  for target_language in ${languages[@]} ; do
+    if [ ${source_language} != 'en' -a ${target_language} != "en" -a ${source_language} != ${target_language} ] ; then
+      bitext="${bucc_edition}.${source_language}-${target_language}.train.extracted.th${threshold}.csv"
       if [ ! -s ${bitext} ] ; then
-        echo "Extracting bitexts for ${lsrc}-${ltrg}"
+        echo "Extracting bitexts for ${source_language}-${target_language}"
         python3 ${LASER}/source/mine_bitexts.py \
-          ${edir}/${bucc}.${lsrc}-en.train.txt.${lsrc} \
-          ${edir}/${bucc}.${ltrg}-en.train.txt.${ltrg} \
-          --src-lang ${lsrc} --trg-lang ${ltrg} \
-          --src-embeddings ${edir}/${bucc}.${lsrc}-en.train.enc.${lsrc} \
-          --trg-embeddings ${edir}/${bucc}.${ltrg}-en.train.enc.${ltrg} \
+          ${normalized_texts_embeddings_directory}/${bucc_edition}.${source_language}-en.train.txt.${source_language} \
+          ${normalized_texts_embeddings_directory}/${bucc_edition}.${target_language}-en.train.txt.${target_language} \
+          --src-lang ${source_language} --trg-lang ${target_language} \
+          --src-embeddings ${normalized_texts_embeddings_directory}/${bucc_edition}.${source_language}-en.train.enc.${source_language} \
+          --trg-embeddings ${normalized_texts_embeddings_directory}/${bucc_edition}.${target_language}-en.train.enc.${target_language} \
           --unify --mode mine --retrieval max --margin ratio -k 4  \
-          --output ${bitext} --threshold ${th} \
+          --output ${bitext} --threshold ${threshold} \
           --verbose --gpu
       fi
     fi
