5b388a3272e2547bde31ad69116569db3ca49040
jrfilocao <bla@email.com>
2019-09-25 01:12:26 +0200

    Improve script for finding articles showing potential ones

--- tasks/news_uni_leipzig/find_articles.py
+++ tasks/news_uni_leipzig/find_articles.py
@@ -1,18 +1,28 @@
 #!/bin/python3
 
-input_file = open("wdt_2019-07-08_deu.source", "r")
-file_lines = input_file.readlines()
+ARTICLE_MINIMUM_LINE_COUNT = 5
+ARTICLE_MINIMUM_AVERAGE_LINE_WORD_COUNT = 15
 
-# for line in file_lines:
-#     if not line.strip():
-#         print("empty string")
-#     else:
-#         print("\n")
+with open("wdt_2019-07-08_deu.source", "r") as input_file:
+    file_lines = input_file.readlines()
 
-for line in file_lines:
-    if not line.strip():
+articles = []
+article = []
+for file_line in file_lines:
+    if not file_line.strip():
         print("empty string")
+        article_line_count = len(article)
+        if article_line_count >= ARTICLE_MINIMUM_LINE_COUNT:
+            article_word_count = 0
+            for article_line in article:
+                article_line_word_count = len(article_line.split())
+                article_word_count += article_line_word_count
+            article_average_line_word_count = article_word_count / article_line_count
+            if article_average_line_word_count > ARTICLE_MINIMUM_AVERAGE_LINE_WORD_COUNT:
+                print(article)
+                articles.append(article)
+            print(article_average_line_word_count)
+        article = []
     else:
-        print(len(line.split()))
-
-
+        article.append(file_line)
+        print(len(file_line.split()))
