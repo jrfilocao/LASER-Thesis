66327b315349993f6cafca7e5e358e57211f9b69
jrfilocao <bla@email.com>
2019-10-07 01:02:58 +0200

    Refactor main arguments and print example article for segmentation

--- tasks/news_uni_leipzig/find_all_articles.py
+++ tasks/news_uni_leipzig/find_all_articles.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 
 import argparse
+import syntok.segmenter as segmenter
 from finding_articles import find_articles
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Finding articles in all languages')
-    parser.add_argument('--file-name', required=True,
-                        help='news articles file name')
-    parser.add_argument('--line-count', type=int, required=True,
-                        help='minimum line count of an article')
-    parser.add_argument('--average-line-word-count', type=int, required=True,
-                        help='minimum average word count of a line in a article')
-    args = parser.parse_args()
 
-    find_articles(args.file_name, args.line_count, args.average_line_word_count)
+def get_argumet_parser():
+    parser = argparse.ArgumentParser(description='Finding articles for all languages in crawled news texts')
+    parser.add_argument('--input-file-name', required=True,
+                        help='input file name of news articles in a specific language')
+    parser.add_argument('--line-count', type=int, required=True,
+                        help='minimum count of consecutive lines to form an article')
+    parser.add_argument('--average-line-word-count', type=int, required=True,
+                        help='minimum average word count in a line to form an article')
+    return parser
+
+
+if __name__ == "__main__":
+    parser = get_argumet_parser()
+    arguments = parser.parse_args()
+    articles = find_articles(arguments.file_name, arguments.line_count, arguments.average_line_word_count)
+
--- tasks/news_uni_leipzig/finding_articles.py
+++ tasks/news_uni_leipzig/finding_articles.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 
-def find_articles(file_name, article_minimum_line_count, article_minimum_average_line_word_count):
+def find_articles(file_name, minimum_number_of_consecutive_lines, minimum_average_word_count_of_a_line):
     file_lines = _get_file_lines(file_name)
-    articles = _parse_articles(file_lines, article_minimum_line_count, article_minimum_average_line_word_count)
+    articles = _parse_articles(file_lines, minimum_number_of_consecutive_lines, minimum_average_word_count_of_a_line)
     _write_article_count_to_console(articles)
+    _write_articles_to_file(articles)
+    return articles
 
 
 def _get_file_lines(file_name):
@@ -13,34 +15,34 @@ def _get_file_lines(file_name):
     return file_lines
 
 
-def _parse_articles(file_lines, article_minimum_line_count, article_minimum_average_line_word_count):
+def _parse_articles(file_lines, minimum_number_of_consecutive_lines, minimum_average_word_count_of_a_line):
     articles = []
     article = []
     for file_line in file_lines:
-        _get_articles_with_minimum_line_and_word_count(article, articles, file_line, article_minimum_line_count,
-                                                       article_minimum_average_line_word_count)
+        if not file_line.strip():
+            article_line_count = len(article)
+            if article_line_count >= minimum_number_of_consecutive_lines:
+                article_word_count = 0
+                for article_line in article:
+                    article_line_word_count = len(article_line.split())
+                    article_word_count += article_line_word_count
+                article_average_line_word_count = article_word_count / article_line_count
+                if article_average_line_word_count > minimum_average_word_count_of_a_line:
+                    print("\n")
+                    print(article)
+                    articles.append(article)
+                    print(article_average_line_word_count)
+            article = []
+        else:
+            article.append(file_line)
     return articles
-
-
-def _get_articles_with_minimum_line_and_word_count(article, articles, file_line, article_minimum_line_count,
-                                                   article_minimum_average_line_word_count):
-    if not file_line.strip():
-        article_line_count = len(article)
-        if article_line_count >= article_minimum_line_count:
-            article_word_count = 0
-            for article_line in article:
-                article_line_word_count = len(article_line.split())
-                article_word_count += article_line_word_count
-            article_average_line_word_count = article_word_count / article_line_count
-            if article_average_line_word_count > article_minimum_average_line_word_count:
-                print("\n")
-                print(article)
-                articles.append(article)
-                print(article_average_line_word_count)
-        article.clear()
-    else:
-        article.append(file_line)
 
 
 def _write_article_count_to_console(articles):
     print("Total articles found", len(articles))
+
+
+def _write_articles_to_file(articles):
+    with open('your_file.txt', 'w') as output_file:
+        for line in articles[len(articles) - 1]:
+            output_file.write(line)
