6de95f5831c6060eea70ca367f57b26a97e4d1f1
jrfilocao <bla@email.com>
2019-11-24 23:25:32 +0100

    Fix parameter passing for embedding and mining functions

--- tasks/news_uni_leipzig/parallel_sentences_miner.sh
+++ tasks/news_uni_leipzig/parallel_sentences_miner.sh
@@ -27,14 +27,11 @@ find_sentences () {
 }
 
 embed_sentences () {
-  sentence_base_file_name=$1
-  language=$2
-
-  sentence_file_name="${sentence_base_file_name}_${language}_sentences"
-  sentence_embedding_output_file_name="${language}_embedding"
+  sentences_file_path="${output_directory}/${language}_sentences"
+  sentence_embedding_output_file_name="${output_directory}/${language}_embedding"
 
   if [ ! -s ${sentence_embedding_output_file_name} ] ; then
-    cat ${sentence_file_name} | python3 ${LASER}/source/embed.py \
+    cat ${sentences_file_path} | python3 ${LASER}/source/embed.py \
       --encoder ${encoder} \
       --token-lang ${language} \
       --bpe-codes ${bpe_codes} \
@@ -44,16 +41,13 @@ embed_sentences () {
 }
 
 mine_for_bitexts () {
-  sentence_base_file_name=$1
-  source_language=$2
-  target_language=$3
   threshold=1.1
-  sentence_candidates="${base_file_name}_${source_language}_${target_language}_sentence_candidates.tsv"
+  sentence_candidates="${output_directory}/${source_language}_${target_language}_sentence_candidates.tsv"
   if [ ! -s ${sentence_candidates} ] ; then
     python3 ${LASER}/source/mine_bitexts.py \
-       ${sentence_base_file_name}_${source_language}_sentences ${sentence_base_file_name}_${target_language}_sentences \
+       ${output_directory}/${source_language}_sentences ${output_directory}/${target_language}_sentences \
        --src-lang ${source_language} --trg-lang ${target_language} \
-       --src-embeddings ${sentence_base_file_name}_${source_language}_embedding --trg-embeddings ${sentence_base_file_name}_${target_language}_embedding \
+       --src-embeddings ${output_directory}/${source_language}_embedding --trg-embeddings ${output_directory}/${target_language}_embedding \
        --unify --mode mine --retrieval max --margin ratio -k 4  \
        --output ${sentence_candidates} --threshold ${threshold} \
        --verbose
@@ -70,9 +64,10 @@ mine_for_bitexts () {
 echo -e "\nProcessing news articles"
 
 input_directory=input_files
+output_directory=output_files
+
 input_base_file_names=(wdt_2019-07-08 wdt_2019-07-09 wdt_2019-07-10 wdt_2019-07-11 wdt_2019-07-12 wdt_2019-07-13 wdt_2019-07-14)
 languages=(en pt de)
-
 
 for input_base_file_name in "${input_base_file_names[@]}"; do
   for language in "${languages[@]}"; do
@@ -81,7 +76,7 @@ for input_base_file_name in "${input_base_file_names[@]}"; do
 done
 
 for language in "${languages[@]}"; do
-  embed_sentences ${root_directory}/ ${language}
+  embed_sentences
 done
 
 language_pairs=( "en de" "en pt" "de pt")
@@ -90,7 +85,5 @@ for language_pair in "${language_pairs[@]}"; do
   IFS=' ' read -r -a language_pair_array <<< "$language_pair"
   source_language="${language_pair_array[0]}"
   target_language="${language_pair_array[1]}"
-
-  mine_for_bitexts ${root_directory}/ ${source_language} ${target_language}
-
+  mine_for_bitexts
 done
