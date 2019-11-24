186f680db4d35451c040194d34d67d1d00121d32
jrfilocao <bla@email.com>
2019-11-24 22:17:03 +0100

    Check if prediction array for sentence language is empty to avoid error

--- tasks/news_uni_leipzig/language_identification.py
+++ tasks/news_uni_leipzig/language_identification.py
@@ -4,6 +4,9 @@ from pyfasttext import FastText
 
 MODEL = FastText('lid.176.ftz')
 
+
 def is_sentence_language_not_correct(sentence: str, language: str):
     prediction = MODEL.predict_proba_single(sentence, k=1)
+    if len(prediction) == 0 or len(prediction[0]) == 0:
+        return False
     return prediction[0][0] != language
