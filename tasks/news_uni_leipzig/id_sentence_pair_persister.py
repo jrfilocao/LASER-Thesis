2b7b38b78a6c84ba28c19cca1f0f4720d8d04253
jrfilocao <bla@email.com>
2020-01-19 15:46:34 +0100

    Substitute database and repository methods

--- tasks/news_uni_leipzig/id_sentence_pair_persister.py
+++ tasks/news_uni_leipzig/id_sentence_pair_persister.py
@@ -1,20 +1,14 @@
 #!/usr/bin/python3
 
 import argparse
-import psycopg2
-
-
-INSERT_SENTENCE_SQL = """INSERT INTO sentence(sentence_id, article_id, sentence) VALUES(%s, %s, %s);"""
+from database_connector import get_database_connection
+from sentence_repository import insert_sentence
 
 
 def _get_argument_parser():
     parser = argparse.ArgumentParser(description='Persisting sentences from files in Redis')
     parser.add_argument('--sentence-files', nargs='+', help='sentence files to be processed', required=True)
     return parser
-
-
-def _get_database_connection():
-    return psycopg2.connect(host="database", database="postgres", user="postgres", password="postgres")
 
 
 def _get_id_sentence(id_sentence_pair):
@@ -29,20 +23,13 @@ def _get_article_id(sentence_id):
     return sentence_id[:article_end_index]
 
 
-def _insert_sentence(sentence_id, article_id, sentence, database_cursor):
-    try:
-        database_cursor.execute(INSERT_SENTENCE_SQL, (sentence_id, article_id, sentence))
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-
-
 if __name__ == "__main__":
 
     parser = _get_argument_parser()
     arguments = parser.parse_args()
 
     try:
-        database_connection = _get_database_connection()
+        database_connection = get_database_connection()
         database_cursor = database_connection.cursor()
 
         for sentence_file in arguments.sentence_files:
@@ -52,7 +39,7 @@ if __name__ == "__main__":
                     try:
                         sentence_id, sentence = _get_id_sentence(id_sentence_pair)
                         article_id = _get_article_id(sentence_id)
-                        _insert_sentence(sentence_id, article_id, sentence, database_cursor)
+                        insert_sentence(sentence_id, article_id, sentence, database_cursor)
                     except ValueError:
                         pass
             database_connection.commit()
