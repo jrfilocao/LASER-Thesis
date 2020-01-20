478a2dd52e0c409583b2fec74f8a75535bd65ada
jrfilocao <bla@email.com>
2020-01-20 12:17:23 +0100

    Remove sentence ids since this information is not present in this phase

--- docker/database/init.sql
+++ docker/database/init.sql
@@ -5,8 +5,6 @@ CREATE TABLE sentence (
 );
 
 CREATE TABLE matched_article (
-   source_sentence_id VARCHAR (200) NOT NULL,
-   target_sentence_id VARCHAR (200) NOT NULL,
    source_article_id VARCHAR (200) NOT NULL,
    target_article_id VARCHAR (200) NOT NULL,
    source_sentence VARCHAR (5000) NOT NULL,
