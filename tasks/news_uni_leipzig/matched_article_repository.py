b5608a297e94fa8010c3bd123adb714f5d952b3b
jrfilocao <bla@email.com>
2020-01-20 12:18:07 +0100

    Add repository for matched article table

---
+++ tasks/news_uni_leipzig/matched_article_repository.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+import psycopg2
+
+
+INSERT_MATCHED_ARTICLE_SQL = """INSERT INTO matched_article(source_article_id,
+                                                            target_article_id,
+                                                            source_sentence,
+                                                            target_sentence,
+                                                            source_article_text,
+                                                            target_article_text,
+                                                            named_entities_score) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+
+
+def insert_matched_article(source_article_id,
+                           target_article_id,
+                           source_sentence,
+                           target_sentence,
+                           source_article_text,
+                           target_article_text,
+                           named_entities_score,
+                           database_cursor):
+
+    try:
+        database_cursor.execute(INSERT_MATCHED_ARTICLE_SQL, (source_article_id,
+                                                             target_article_id,
+                                                             source_sentence,
+                                                             target_sentence,
+                                                             source_article_text,
+                                                             target_article_text,
+                                                             named_entities_score))
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
