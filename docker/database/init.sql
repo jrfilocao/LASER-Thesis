0d8d2a194100ce9a9cd563cb356c110aca59844c
jrfilocao <bla@email.com>
2020-01-20 12:02:00 +0100

    Add table storing matched articles

--- docker/database/init.sql
+++ docker/database/init.sql
@@ -1,5 +1,17 @@
-CREATE TABLE sentence(
+CREATE TABLE sentence (
    sentence_id VARCHAR (200) PRIMARY KEY,
    article_id VARCHAR (200) NOT NULL,
    sentence VARCHAR (5000) NOT NULL
+);
+
+CREATE TABLE matched_article (
+   source_sentence_id VARCHAR (200) NOT NULL,
+   target_sentence_id VARCHAR (200) NOT NULL,
+   source_article_id VARCHAR (200) NOT NULL,
+   target_article_id VARCHAR (200) NOT NULL,
+   source_sentence VARCHAR (5000) NOT NULL,
+   target_sentence VARCHAR (5000) NOT NULL,
+   source_article_text TEXT NOT NULL,
+   target_article_text TEXT NOT NULL,
+   named_entities_score TEXT
 );
