dffb30bb9fa8b006ffdb15d037a7643ec2e7eb75
jrfilocao <bla@email.com>
2020-02-21 17:59:06 +0100

    Complete report creator and repository

--- tasks/news_uni_leipzig/report_creator.py
+++ tasks/news_uni_leipzig/report_creator.py
@@ -1,5 +1,5 @@
 from database_connector import get_database_connection
-from report_repository import get_total_sentence_pairs_count
+from report_repository import *
 
 
 class ReportEntry:
@@ -8,15 +8,101 @@ class ReportEntry:
         self.value = value
 
     def __repr__(self):
-        return self.key + ' ' + self.value
+        return self.key + ' ' + self.value + '\n'
 
     def __str__(self):
-        return self.key + ' ' + self.value
+        return self.key + ' ' + self.value + '\n'
 
 
 def create_report(database_cursor):
-    total_sentence_pairs_count = get_total_sentence_pairs_count(database_cursor)
-    return ReportEntry('total sentence pairs', str(total_sentence_pairs_count))
+    report_entries = []
+    report_entries.extend(get_total_report_entries(database_cursor))
+    report_entries.extend(_get_en_de_report_entries(database_cursor))
+    report_entries.extend(_get_en_pt_report_entries(database_cursor))
+    report_entries.extend(_get_de_pt_report_entries(database_cursor))
+    return report_entries
+
+
+def _get_de_pt_report_entries(database_cursor):
+    entries = []
+    entries.append(ReportEntry('*********************', 'DE-PT'))
+    entries.append(ReportEntry('potential_similar_sentences_de_pt', str(get_potential_similar_sentences_de_pt(database_cursor))))
+    unique_article_pairs_de_pt = get_unique_article_pairs_de_pt(database_cursor)
+    unique_article_pairs_with_common_named_entities_de_pt = get_unique_article_pairs_with_common_named_entities_de_pt(database_cursor)
+    unique_articles_with_common_named_entities_and_multiple_similar_sentences_de_pt = \
+        get_unique_articles_with_common_named_entities_and_multiple_similar_sentences_de_pt(database_cursor)
+    unique_article_pairs_with_common_named_entities_de_pt_percentage = float(unique_article_pairs_with_common_named_entities_de_pt) / float(unique_article_pairs_de_pt) * 100
+    unique_articles_with_common_named_entities_and_multiple_similar_sentences_de_pt_percentage = \
+        float(unique_articles_with_common_named_entities_and_multiple_similar_sentences_de_pt) / float(unique_article_pairs_de_pt) * 100
+    entries.append(ReportEntry('unique_article_pairs_de_pt', str(unique_article_pairs_de_pt)))
+    entries.append(ReportEntry('unique_article_pairs_with_common_named_entities_de_pt', str(unique_article_pairs_with_common_named_entities_de_pt)))
+    entries.append(ReportEntry('unique_article_pairs_with_common_named_entities_de_pt_percentage', str(unique_article_pairs_with_common_named_entities_de_pt_percentage)))
+    entries.append(ReportEntry('unique_articles_with_common_named_entities_and_multiple_similar_sentences_de_pt',
+                                      str(unique_articles_with_common_named_entities_and_multiple_similar_sentences_de_pt)))
+    entries.append(ReportEntry('unique_articles_with_common_named_entities_and_multiple_similar_sentences_de_pt_percentage',
+                                      str(unique_articles_with_common_named_entities_and_multiple_similar_sentences_de_pt_percentage)))
+    entries.append(ReportEntry('unique_articles_with_more_than_2_sentences_de_pt', str(get_unique_articles_with_more_than_2_sentences_de_pt(database_cursor))))
+    return entries
+
+
+def _get_en_pt_report_entries(database_cursor):
+    entries = []
+    entries.append(ReportEntry('*********************', 'EN-PT'))
+    entries.append(ReportEntry('potential_similar_sentences_en_pt', str(get_potential_similar_sentences_en_pt(database_cursor))))
+
+    unique_article_pairs_en_pt = get_unique_article_pairs_en_pt(database_cursor)
+    unique_article_pairs_with_common_named_entities_en_pt = get_unique_article_pairs_with_common_named_entities_en_pt(database_cursor)
+    unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_pt = \
+        get_unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_pt(database_cursor)
+    unique_article_pairs_with_common_named_entities_en_pt_percentage = float(unique_article_pairs_with_common_named_entities_en_pt) / float(unique_article_pairs_en_pt) * 100
+    unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_pt_percentage = \
+        float(unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_pt) / float(unique_article_pairs_en_pt) * 100
+    entries.append(ReportEntry('unique_article_pairs_en_pt', str(unique_article_pairs_en_pt)))
+    entries.append(ReportEntry('unique_article_pairs_with_common_named_entities_en_pt', str(unique_article_pairs_with_common_named_entities_en_pt)))
+    entries.append(ReportEntry('unique_article_pairs_with_common_named_entities_en_pt_percentage', str(unique_article_pairs_with_common_named_entities_en_pt_percentage)))
+    entries.append(ReportEntry('unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_pt',
+                                      str(get_unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_pt(database_cursor))))
+    entries.append(ReportEntry('unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_pt_percentage',
+                                      str(unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_pt_percentage)))
+    entries.append(ReportEntry('unique_articles_with_more_than_2_sentences_en_pt', str(get_unique_articles_with_more_than_2_sentences_en_pt(database_cursor))))
+    return entries
+
+
+def _get_en_de_report_entries(database_cursor):
+    entries = []
+    entries.append(ReportEntry('*********************', 'EN-DE'))
+    entries.append(ReportEntry('potential_similar_sentences_en_de', str(get_potential_similar_sentences_en_de(database_cursor))))
+    unique_article_pairs_en_de = get_unique_article_pairs_en_de(database_cursor)
+    unique_article_pairs_with_common_named_entities_en_de = get_unique_article_pairs_with_common_named_entities_en_de(database_cursor)
+    unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_de = \
+        get_unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_de(database_cursor)
+    unique_article_pairs_with_common_named_entities_en_de_percentage = float(unique_article_pairs_with_common_named_entities_en_de) / float(unique_article_pairs_en_de) * 100
+    unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_de_percentage = \
+        float(unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_de) / float(unique_article_pairs_en_de) * 100
+    entries.append(ReportEntry('unique_article_pairs_en_de', str(unique_article_pairs_en_de)))
+    entries.append(ReportEntry('unique_article_pairs_with_common_named_entities_en_de', str(unique_article_pairs_with_common_named_entities_en_de)))
+    entries.append(ReportEntry('unique_article_pairs_with_common_named_entities_en_de_percentage', str(unique_article_pairs_with_common_named_entities_en_de_percentage)))
+    entries.append(ReportEntry('unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_de',
+                                      str(unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_de)))
+    entries.append(ReportEntry('unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_de_percentage',
+                                      str(unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_de_percentage)))
+    entries.append(ReportEntry('unique_articles_with_more_than_2_sentences_en_de', str(get_unique_articles_with_more_than_2_sentences_en_de(database_cursor))))
+    return entries
+
+
+def get_total_report_entries(database_cursor):
+    entries = []
+    entries.append(ReportEntry('total sentence pairs', str(get_total_sentence_pairs_count(database_cursor))))
+    entries.append(ReportEntry('total unique_article_pairs', str(get_total_unique_article_pairs_count(database_cursor))))
+    entries.append(ReportEntry('total_unique_article_pairs_with_common_named_entities', str(get_total_unique_article_pairs_with_common_named_entities(database_cursor))))
+    total_unique_article_pairs_with_more_than_2_sentences = get_total_unique_article_pairs_with_more_than_2_sentences(database_cursor)
+    total_unique_article_pairs_without_common_named_entities_and_with_more_than_2_sentence = \
+        get_total_unique_article_pairs_without_common_named_entities_and_with_more_than_2_sentence(database_cursor)
+    total_unique_article_pairs_with_common_named_entities_and_more_than_2_sentence = total_unique_article_pairs_with_more_than_2_sentences - total_unique_article_pairs_without_common_named_entities_and_with_more_than_2_sentence
+    entries.append(ReportEntry('total_unique_article_pairs_with_common_named_entities_and_more_than_2_sentence', str(total_unique_article_pairs_with_common_named_entities_and_more_than_2_sentence)))
+    entries.append(ReportEntry('total_unique_article_pairs_with_more_than_2_sentences', str(total_unique_article_pairs_with_more_than_2_sentences)))
+    entries.append(ReportEntry('total_unique_article_pairs_without_common_named_entities_and_with_more_than_2_sentence', str(total_unique_article_pairs_without_common_named_entities_and_with_more_than_2_sentence)))
+    return entries
 
 
 if __name__ == "__main__":
--- tasks/news_uni_leipzig/report_repository.py
+++ tasks/news_uni_leipzig/report_repository.py
@@ -162,3 +162,158 @@ def get_total_sentence_pairs_count(database_cursor):
         return result[0]
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
+
+
+def get_total_unique_article_pairs_count(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLE_PAIRS, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_total_unique_article_pairs_with_common_named_entities(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLE_PAIRS_WITH_COMMON_NAMED_ENTITIES, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_total_unique_article_pairs_with_more_than_2_sentences(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLE_PAIRS_WITH_MORE_THAN_2_SENTENCES, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_total_unique_article_pairs_without_common_named_entities_and_with_more_than_2_sentence(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLE_PAIRS_WITHOUT_COMMON_NAMED_ENTITIES_AND_WITH_MORE_THAN_2_SENTENCE, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_potential_similar_sentences_en_de(database_cursor):
+    try:
+        database_cursor.execute(SELECT_TOTAL_POTENTIAL_SIMILAR_SENTENCES_EN_DE, ())
+        result = database_cursor.fetchone()
+        return result[0]
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_potential_similar_sentences_en_pt(database_cursor):
+    try:
+        database_cursor.execute(SELECT_TOTAL_POTENTIAL_SIMILAR_SENTENCES_EN_PT, ())
+        result = database_cursor.fetchone()
+        return result[0]
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_potential_similar_sentences_de_pt(database_cursor):
+    try:
+        database_cursor.execute(SELECT_TOTAL_POTENTIAL_SIMILAR_SENTENCES_DE_PT, ())
+        result = database_cursor.fetchone()
+        return result[0]
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_unique_article_pairs_en_de(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLE_PAIRS_EN_DE, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_unique_article_pairs_en_pt(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLE_PAIRS_EN_PT, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_unique_article_pairs_de_pt(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLE_PAIRS_DE_PT, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_unique_article_pairs_with_common_named_entities_en_de(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLE_PAIRS_WITH_COMMON_NAMED_ENTITIES_EN_DE, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_unique_article_pairs_with_common_named_entities_en_pt(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLE_PAIRS_WITH_COMMON_NAMED_ENTITIES_EN_PT, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_unique_article_pairs_with_common_named_entities_de_pt(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLE_PAIRS_WITH_COMMON_NAMED_ENTITIES_DE_PT, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_unique_articles_with_more_than_2_sentences_en_de(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLES_WITH_MORE_THAN_2_SENTENCES_EN_DE, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_unique_articles_with_more_than_2_sentences_en_pt(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLES_WITH_MORE_THAN_2_SENTENCES_EN_PT, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_unique_articles_with_more_than_2_sentences_de_pt(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLES_WITH_MORE_THAN_2_SENTENCES_DE_PT, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_de(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLES_WITH_COMMON_NAMED_ENTITIES_AND_MULTIPLE_SIMILAR_SENTENCES_EN_DE, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_pt(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLES_WITH_COMMON_NAMED_ENTITIES_AND_MULTIPLE_SIMILAR_SENTENCES_EN_PT, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+
+
+def get_unique_articles_with_common_named_entities_and_multiple_similar_sentences_de_pt(database_cursor):
+    try:
+        database_cursor.execute(SELECT_UNIQUE_ARTICLES_WITH_COMMON_NAMED_ENTITIES_AND_MULTIPLE_SIMILAR_SENTENCES_DE_PT, ())
+        return database_cursor.rowcount
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
