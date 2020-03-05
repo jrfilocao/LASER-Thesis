a5e62ce5d29b0b2a82bfc0b787483f564797330a
jrfilocao <bla@email.com>
2020-03-05 10:39:34 +0100

    Format threshold with 3 digits precision

--- tasks/news_uni_leipzig/reporting/report_writer.py
+++ tasks/news_uni_leipzig/reporting/report_writer.py
@@ -1,12 +1,17 @@
+THRESHOLD_PRECISION_FORMAT = '.3f'
+
+
 def write_report_entries_into_csv_file(sentence_pair_score_threshold, report_entries, output_report_base_file_name):
-    file_name = output_report_base_file_name + '_key_value_{}.txt'.format(sentence_pair_score_threshold)
+    formatted_threshold = format(sentence_pair_score_threshold, THRESHOLD_PRECISION_FORMAT)
+    file_name = output_report_base_file_name + '_key_value_{}.txt'.format(formatted_threshold)
     with open(file_name, 'w') as report_file:
         for report_entry in report_entries:
             report_file.write(str(report_entry))
 
 
 def write_article_pair_results_into_file(sentence_pair_score_threshold, result_rows, output_report_base_file_name, language, type):
-    file_name = output_report_base_file_name + '_pairs_{}_{}_{}.txt'.format(language, sentence_pair_score_threshold, type)
+    formatted_threshold = format(sentence_pair_score_threshold, THRESHOLD_PRECISION_FORMAT)
+    file_name = output_report_base_file_name + '_pairs_{}_{}_{}.txt'.format(language, formatted_threshold, type)
     with open(file_name, 'w') as report_file:
         for result_row in result_rows:
             report_file.write(str(result_row) + '\n')
