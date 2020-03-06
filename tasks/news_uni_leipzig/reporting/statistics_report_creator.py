61ea302bcb8f8936d2670aadc757f122272dddcd
jrfilocao <bla@email.com>
2020-03-06 17:26:20 +0100

    Remove class report entry and using tuple instead

--- tasks/news_uni_leipzig/reporting/statistics_report_creator.py
+++ tasks/news_uni_leipzig/reporting/statistics_report_creator.py
@@ -9,7 +9,6 @@ sys.path.append(NEWS_TASK + '/reporting')
 from database_connector import get_database_connection
 from report_repository import *
 from report_writer import *
-from report_entry import ReportEntry
 
 
 def _create_statistics_report(sentence_pair_score_threshold, database_cursor):
@@ -36,8 +35,8 @@ def _create_statistics_report(sentence_pair_score_threshold, database_cursor):
 
 def _get_de_pt_report_entries(sentence_pair_score_threshold, database_cursor):
     entries = []
-    entries.append(ReportEntry('language', 'DE-PT'))
-    entries.append(ReportEntry('sentence pairs de_pt', str(get_potential_similar_sentences_de_pt(sentence_pair_score_threshold, database_cursor))))
+    entries.append(('language', 'DE-PT'))
+    entries.append(('sentence pairs de_pt', str(get_potential_similar_sentences_de_pt(sentence_pair_score_threshold, database_cursor))))
     unique_article_pairs_de_pt = get_unique_article_pairs_de_pt(sentence_pair_score_threshold, database_cursor)
 
     if unique_article_pairs_de_pt == 0:
@@ -48,22 +47,22 @@ def _get_de_pt_report_entries(sentence_pair_score_threshold, database_cursor):
     unique_article_pairs_with_common_named_entities_de_pt_percentage = float(unique_article_pairs_with_common_named_entities_de_pt) / float(unique_article_pairs_de_pt) * 100
     unique_articles_with_common_named_entities_and_multiple_similar_sentences_de_pt_percentage = \
         float(unique_articles_with_common_named_entities_and_multiple_similar_sentences_de_pt) / float(unique_article_pairs_de_pt) * 100
-    entries.append(ReportEntry('unique article pairs de_pt', str(unique_article_pairs_de_pt)))
-    entries.append(ReportEntry('unique article pairs with common named entities de_pt', str(unique_article_pairs_with_common_named_entities_de_pt)))
-    entries.append(ReportEntry('unique article pairs with common named entities de_pt percentage', str(unique_article_pairs_with_common_named_entities_de_pt_percentage)))
-    entries.append(ReportEntry('unique articles with common named entities and multiple similar sentences de_pt',
+    entries.append(('unique article pairs de_pt', str(unique_article_pairs_de_pt)))
+    entries.append(('unique article pairs with common named entities de_pt', str(unique_article_pairs_with_common_named_entities_de_pt)))
+    entries.append(('unique article pairs with common named entities de_pt percentage', str(unique_article_pairs_with_common_named_entities_de_pt_percentage)))
+    entries.append(('unique articles with common named entities and multiple similar sentences de_pt',
                                str(unique_articles_with_common_named_entities_and_multiple_similar_sentences_de_pt)))
-    entries.append(ReportEntry('unique articles with common named entities and multiple similar sentences de_pt percentage',
+    entries.append(('unique articles with common named entities and multiple similar sentences de_pt percentage',
                                str(unique_articles_with_common_named_entities_and_multiple_similar_sentences_de_pt_percentage)))
     entries.append(
-        ReportEntry('unique articles with more than 2 sentences de_pt', str(get_unique_articles_with_more_than_2_sentences_de_pt(sentence_pair_score_threshold, database_cursor))))
+        ('unique articles with more than 2 sentences de_pt', str(get_unique_articles_with_more_than_2_sentences_de_pt(sentence_pair_score_threshold, database_cursor))))
     return entries
 
 
 def _get_en_pt_report_entries(sentence_pair_score_threshold, database_cursor):
     entries = []
-    entries.append(ReportEntry('language', 'EN-PT'))
-    entries.append(ReportEntry('sentence pairs en_pt', str(get_potential_similar_sentences_en_pt(sentence_pair_score_threshold, database_cursor))))
+    entries.append(('language', 'EN-PT'))
+    entries.append(('sentence pairs en_pt', str(get_potential_similar_sentences_en_pt(sentence_pair_score_threshold, database_cursor))))
     unique_article_pairs_en_pt = get_unique_article_pairs_en_pt(sentence_pair_score_threshold, database_cursor)
 
     if unique_article_pairs_en_pt == 0:
@@ -74,22 +73,22 @@ def _get_en_pt_report_entries(sentence_pair_score_threshold, database_cursor):
     unique_article_pairs_with_common_named_entities_en_pt_percentage = float(unique_article_pairs_with_common_named_entities_en_pt) / float(unique_article_pairs_en_pt) * 100
     unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_pt_percentage = \
         float(unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_pt) / float(unique_article_pairs_en_pt) * 100
-    entries.append(ReportEntry('unique article pairs en_pt', str(unique_article_pairs_en_pt)))
-    entries.append(ReportEntry('unique article pairs with common named entities en_pt', str(unique_article_pairs_with_common_named_entities_en_pt)))
-    entries.append(ReportEntry('unique article pairs with common named entities en_pt percentage', str(unique_article_pairs_with_common_named_entities_en_pt_percentage)))
-    entries.append(ReportEntry('unique articles with common named entities and multiple similar sentences en_pt',
+    entries.append(('unique article pairs en_pt', str(unique_article_pairs_en_pt)))
+    entries.append(('unique article pairs with common named entities en_pt', str(unique_article_pairs_with_common_named_entities_en_pt)))
+    entries.append(('unique article pairs with common named entities en_pt percentage', str(unique_article_pairs_with_common_named_entities_en_pt_percentage)))
+    entries.append(('unique articles with common named entities and multiple similar sentences en_pt',
                                str(get_unique_articles_count_with_common_named_entities_and_multiple_similar_sentences_en_pt(sentence_pair_score_threshold, database_cursor))))
-    entries.append(ReportEntry('unique articles with common named entities and multiple similar sentences en_pt percentage',
+    entries.append(('unique articles with common named entities and multiple similar sentences en_pt percentage',
                                str(unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_pt_percentage)))
     entries.append(
-        ReportEntry('unique articles with more than 2 sentences en_pt', str(get_unique_articles_with_more_than_2_sentences_en_pt(sentence_pair_score_threshold, database_cursor))))
+        ('unique articles with more than 2 sentences en_pt', str(get_unique_articles_with_more_than_2_sentences_en_pt(sentence_pair_score_threshold, database_cursor))))
     return entries
 
 
 def _get_en_de_report_entries(sentence_pair_score_threshold, database_cursor):
     entries = []
-    entries.append(ReportEntry('language', 'EN-DE'))
-    entries.append(ReportEntry('sentence pairs en_de', str(get_potential_similar_sentences_en_de(sentence_pair_score_threshold, database_cursor))))
+    entries.append(('language', 'EN-DE'))
+    entries.append(('sentence pairs en_de', str(get_potential_similar_sentences_en_de(sentence_pair_score_threshold, database_cursor))))
     unique_article_pairs_en_de = get_unique_article_pairs_en_de(sentence_pair_score_threshold, database_cursor)
 
     if unique_article_pairs_en_de == 0:
@@ -100,32 +99,32 @@ def _get_en_de_report_entries(sentence_pair_score_threshold, database_cursor):
     unique_article_pairs_with_common_named_entities_en_de_percentage = float(unique_article_pairs_with_common_named_entities_en_de) / float(unique_article_pairs_en_de) * 100
     unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_de_percentage = \
         float(unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_de) / float(unique_article_pairs_en_de) * 100
-    entries.append(ReportEntry('unique article pairs en_de', str(unique_article_pairs_en_de)))
-    entries.append(ReportEntry('unique article pairs with common named entities en_de', str(unique_article_pairs_with_common_named_entities_en_de)))
-    entries.append(ReportEntry('unique article pairs with common named entities en_de percentage', str(unique_article_pairs_with_common_named_entities_en_de_percentage)))
-    entries.append(ReportEntry('unique articles with common named entities and multiple similar sentences en_de',
+    entries.append(('unique article pairs en_de', str(unique_article_pairs_en_de)))
+    entries.append(('unique article pairs with common named entities en_de', str(unique_article_pairs_with_common_named_entities_en_de)))
+    entries.append(('unique article pairs with common named entities en_de percentage', str(unique_article_pairs_with_common_named_entities_en_de_percentage)))
+    entries.append(('unique articles with common named entities and multiple similar sentences en_de',
                                str(unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_de)))
-    entries.append(ReportEntry('unique articles with common named entities and multiple similar sentences en_de percentage',
+    entries.append(('unique articles with common named entities and multiple similar sentences en_de percentage',
                                str(unique_articles_with_common_named_entities_and_multiple_similar_sentences_en_de_percentage)))
     entries.append(
-        ReportEntry('unique articles with more than 2 sentences en_de', str(get_unique_articles_with_more_than_2_sentences_en_de(sentence_pair_score_threshold, database_cursor))))
+        ('unique articles with more than 2 sentences en_de', str(get_unique_articles_with_more_than_2_sentences_en_de(sentence_pair_score_threshold, database_cursor))))
     return entries
 
 
 def _get_total_report_entries(sentence_pair_score_threshold, database_cursor):
     entries = []
-    entries.append(ReportEntry('total sentence pairs', str(get_total_sentence_pairs_count(sentence_pair_score_threshold, database_cursor))))
-    entries.append(ReportEntry('total unique article pairs', str(get_total_unique_article_pairs_count(sentence_pair_score_threshold, database_cursor))))
-    entries.append(ReportEntry('total unique article pairs with common named entities',
+    entries.append(('total sentence pairs', str(get_total_sentence_pairs_count(sentence_pair_score_threshold, database_cursor))))
+    entries.append(('total unique article pairs', str(get_total_unique_article_pairs_count(sentence_pair_score_threshold, database_cursor))))
+    entries.append(('total unique article pairs with common named entities',
                                str(get_total_unique_article_pairs_with_common_named_entities(sentence_pair_score_threshold, database_cursor))))
     total_unique_article_pairs_with_more_than_2_sentences = get_total_unique_article_pairs_with_more_than_2_sentences(sentence_pair_score_threshold, database_cursor)
     total_unique_article_pairs_without_common_named_entities_and_with_more_than_2_sentence = \
         get_total_unique_article_pairs_without_common_named_entities_and_with_more_than_2_sentence(sentence_pair_score_threshold, database_cursor)
     total_unique_article_pairs_with_common_named_entities_and_more_than_2_sentence = total_unique_article_pairs_with_more_than_2_sentences - total_unique_article_pairs_without_common_named_entities_and_with_more_than_2_sentence
-    entries.append(ReportEntry('total unique article pairs with common named entities and more than 2 sentence',
+    entries.append(('total unique article pairs with common named entities and more than 2 sentence',
                                str(total_unique_article_pairs_with_common_named_entities_and_more_than_2_sentence)))
-    entries.append(ReportEntry('total unique article pairs with more than 2 sentences', str(total_unique_article_pairs_with_more_than_2_sentences)))
-    entries.append(ReportEntry('total unique article pairs without common named entities and with more than 2 sentence',
+    entries.append(('total unique article pairs with more than 2 sentences', str(total_unique_article_pairs_with_more_than_2_sentences)))
+    entries.append(('total unique article pairs without common named entities and with more than 2 sentence',
                                str(total_unique_article_pairs_without_common_named_entities_and_with_more_than_2_sentence)))
     return entries
 
