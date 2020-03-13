eea0c5f9c4bfc8408467877e92e3902cc852b75a
jrfilocao <bla@email.com>
2020-03-13 20:29:17 +0100

    Turn triple articles unique

--- tasks/news_uni_leipzig/similarity/triple_similarity_finder.py
+++ tasks/news_uni_leipzig/similarity/triple_similarity_finder.py
@@ -32,6 +32,108 @@ def _get_output_article_id(triple_article_index):
     return 'article-' + str(triple_article_index) + '\n'
 
 
+def _get_triple_article_tuple(output_article_id,
+                              en_article_sentence,
+                              pt_article_sentence,
+                              de_article_sentence,
+                              en_article_sentences_text,
+                              pt_article_sentences_text,
+                              de_article_sentences_text):
+    return (output_article_id,
+            [en_article_sentence],
+            [pt_article_sentence],
+            [de_article_sentence],
+            en_article_sentences_text,
+            pt_article_sentences_text,
+            de_article_sentences_text)
+
+
+def _add_triple_sentences_into_articles_if_present(en_article_sentence, pt_article_sentence, de_article_sentence, triple_articles):
+    for triple_article in triple_articles:
+        if en_article_sentence in triple_article[4] or pt_article_sentence in triple_article[5] or de_article_sentence in triple_article[6]:
+            triple_article[1].append(en_article_sentence)
+            triple_article[2].append(pt_article_sentence)
+            triple_article[3].append(de_article_sentence)
+            return True
+    return False
+
+
+def _get_triple_article(en_article_sentence, pt_article_sentence, de_article_sentence, triple_article_index):
+    en_article_sentences = get_sentences_from_first_article_found(en_article_sentence, database_cursor)
+    pt_article_sentences = get_sentences_from_first_article_found(pt_article_sentence, database_cursor)
+    de_article_sentences = get_sentences_from_first_article_found(de_article_sentence, database_cursor)
+    en_article_sentences_text = _get_article_sentences_as_text(en_article_sentences)
+    pt_article_sentences_text = _get_article_sentences_as_text(pt_article_sentences)
+    de_article_sentences_text = _get_article_sentences_as_text(de_article_sentences)
+    output_article_id = _get_output_article_id(triple_article_index)
+    return _get_triple_article_tuple(output_article_id,
+                                     en_article_sentence,
+                                     pt_article_sentence,
+                                     de_article_sentence,
+                                     en_article_sentences_text,
+                                     pt_article_sentences_text,
+                                     de_article_sentences_text)
+
+
+def _write_triple_articles(triple_articles, en_output_file, pt_output_file, de_output_file):
+    for triple_article in triple_articles:
+        article_id = triple_article[0]
+        en_output_file.write(article_id)
+        pt_output_file.write(article_id)
+        de_output_file.write(article_id)
+
+        en_output_file.write('-------\n')
+        pt_output_file.write('-------\n')
+        de_output_file.write('-------\n')
+
+        en_article_sentences_text = triple_article[4]
+        pt_article_sentences_text = triple_article[5]
+        de_article_sentences_text = triple_article[6]
+
+        en_output_file.write(en_article_sentences_text)
+        pt_output_file.write(pt_article_sentences_text)
+        de_output_file.write(de_article_sentences_text)
+
+        en_output_file.write('\n\n')
+        pt_output_file.write('\n\n')
+        de_output_file.write('\n\n')
+        en_output_file.write('-------\n')
+        pt_output_file.write('-------\n')
+        de_output_file.write('-------\n')
+
+        en_article_sentences = triple_article[1]
+        pt_article_sentences = triple_article[2]
+        de_article_sentences = triple_article[3]
+
+        for en_article_sentence in en_article_sentences:
+            en_output_file.write(en_article_sentence + '\n')
+            en_output_file.write('-------\n')
+        for pt_article_sentence in pt_article_sentences:
+            pt_output_file.write(pt_article_sentence + '\n')
+            pt_output_file.write('-------\n')
+        for de_article_sentence in de_article_sentences:
+            de_output_file.write(de_article_sentence + '\n')
+            de_output_file.write('-------\n')
+
+        en_output_file.write('\n\n\n')
+        pt_output_file.write('\n\n\n')
+        de_output_file.write('\n\n\n')
+
+
+def _get_triple_articles(triple_article_sentences):
+    triple_articles = []
+    for triple_article_index, triple_article_sentence in enumerate(triple_article_sentences, start=1):
+        sentences_added_in_previous_articles = _add_triple_sentences_into_articles_if_present(triple_article_sentence[0],
+                                                                                              triple_article_sentence[1],
+                                                                                              triple_article_sentence[2],
+                                                                                              triple_articles)
+
+        if not sentences_added_in_previous_articles:
+            triple_article_tuple = _get_triple_article(triple_article_sentence[0], triple_article_sentence[1], triple_article_sentence[2], triple_article_index)
+            triple_articles.append(triple_article_tuple)
+    return triple_articles
+
+
 if __name__ == "__main__":
     parser = _get_argument_parser()
     arguments = parser.parse_args()
@@ -49,46 +151,9 @@ if __name__ == "__main__":
              open(en_output_file_name, 'w') as en_output_file:
 
             triple_article_sentences = get_triple_similar_article_sentences(database_cursor)
+            triple_articles = _get_triple_articles(triple_article_sentences)
+            _write_triple_articles(triple_articles, en_output_file, pt_output_file, de_output_file)
 
-            for triple_article_index, triple_article_sentence in enumerate(triple_article_sentences, start=1):
-                en_article_sentences = get_sentences_from_first_article_found(triple_article_sentence[0], database_cursor)
-                pt_article_sentences = get_sentences_from_first_article_found(triple_article_sentence[1], database_cursor)
-                de_article_sentences = get_sentences_from_first_article_found(triple_article_sentence[2], database_cursor)
-
-                en_article_sentences_text = _get_article_sentences_as_text(en_article_sentences)
-                pt_article_sentences_text = _get_article_sentences_as_text(pt_article_sentences)
-                de_article_sentences_text = _get_article_sentences_as_text(de_article_sentences)
-
-                output_article_id= _get_output_article_id(triple_article_index)
-
-                en_output_file.write(output_article_id)
-                pt_output_file.write(output_article_id)
-                de_output_file.write(output_article_id)
-                en_output_file.write('-------\n')
-                pt_output_file.write('-------\n')
-                de_output_file.write('-------\n')
-
-                en_output_file.write(en_article_sentences_text)
-                pt_output_file.write(pt_article_sentences_text)
-                de_output_file.write(de_article_sentences_text)
-
-                en_output_file.write('\n\n')
-                pt_output_file.write('\n\n')
-                de_output_file.write('\n\n')
-                en_output_file.write('-------\n')
-                pt_output_file.write('-------\n')
-                de_output_file.write('-------\n')
-
-                en_output_file.write(triple_article_sentence[0])
-                pt_output_file.write(triple_article_sentence[1])
-                de_output_file.write(triple_article_sentence[2])
-
-                en_output_file.write('\n-------')
-                pt_output_file.write('\n-------')
-                de_output_file.write('\n-------')
-                en_output_file.write('\n\n\n')
-                pt_output_file.write('\n\n\n')
-                de_output_file.write('\n\n\n')
 
     finally:
         if database_connection is not None:
