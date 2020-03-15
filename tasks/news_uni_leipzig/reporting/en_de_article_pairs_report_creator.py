41720bf9b005f01c33377f9949583206c8e80032
jrfilocao <bla@email.com>
2020-03-15 17:48:03 +0100

    Fix Recall value dividing by number of extracted articles / 2

--- tasks/news_uni_leipzig/reporting/en_de_article_pairs_report_creator.py
+++ tasks/news_uni_leipzig/reporting/en_de_article_pairs_report_creator.py
@@ -62,7 +62,8 @@ def create_en_de_article_pairs_report(score_threshold, database_cursor, output_r
                                          INVALID_ONLY_NAMED_ENTITY)
 
     report_entries.append('number_of_articles_extracted', total_number_of_articles)
-    report_entries.append('only_named_entity_en_de_recall', float(only_named_entity_correct_article_pairs_count)/float(total_number_of_articles)*100)
+    report_entries.append('number_of_articles_extracted per language', total_number_of_articles/2)
+    report_entries.append('only_named_entity_en_de_recall', float(only_named_entity_correct_article_pairs_count)/float(total_number_of_articles/2)*100)
     report_entries.append('only_named_entity_en_de_precision', float(only_named_entity_correct_article_pairs_count) / float(len(only_named_entity_result_rows)) * 100)
 
     write_article_pair_results_into_file(score_threshold,
