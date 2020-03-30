6e4e59e4f803292f31f69694b6a2e59b71fd1b61
jrfilocao <bla@email.com>
2020-03-30 21:37:57 +0200

    Adapt id sentence extractor of de pt

--- tasks/news_uni_leipzig/extraction/de_pt_corpus/de_pt_article_id_sentence_extractor.py
+++ tasks/news_uni_leipzig/extraction/de_pt_corpus/de_pt_article_id_sentence_extractor.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
 
+from xml.dom import minidom
+
+SENTENCE_TAG = 's'
+
 EMPTY_STRING = ''
 NEW_LINE = '\n'
 
 
 def extract_single_article_from_file(input_file_name):
-    with open(input_file_name, 'r', errors='replace') as input_file:
+    xml_file = minidom.parse(input_file_name)
+    sentences = xml_file.getElementsByTagName(SENTENCE_TAG)
 
-        article_sentences = []
-        file_lines = input_file.readlines()
-
-        for file_line in file_lines:
-            sentence_with_no_new_line = file_line.replace(NEW_LINE, EMPTY_STRING)
-            sentence_without_quote_signs = _remove_all_quote_signs(sentence_with_no_new_line)
-            article_sentences.append(sentence_without_quote_signs)
+    article_sentences = []
+    for sentence in sentences:
+        sentence_text = sentence.firstChild.data
+        sentence_text_without_quote_signs = _remove_all_quote_signs(sentence_text)
+        article_sentences.append(sentence_text_without_quote_signs)
 
     return article_sentences
 
