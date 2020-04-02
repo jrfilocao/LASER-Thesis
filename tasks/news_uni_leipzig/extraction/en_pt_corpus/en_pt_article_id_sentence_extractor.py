4f9f556c7ad895485eb0406fc90361e1ffe3ee14
jrfilocao <bla@email.com>
2020-04-02 21:03:53 +0200

    Fix error and now returning correct meta info sentence

--- tasks/news_uni_leipzig/extraction/en_pt_corpus/en_pt_article_id_sentence_extractor.py
+++ tasks/news_uni_leipzig/extraction/en_pt_corpus/en_pt_article_id_sentence_extractor.py
@@ -16,7 +16,7 @@ def extract_single_article_from_file(input_file_name):
             sentence_without_new_line = file_line.replace(NEW_LINE, EMPTY_STRING)
             sentence_without_quote_signs = _remove_all_quote_signs(sentence_without_new_line)
             sentence_without_leading_spaces = sentence_without_quote_signs.strip()
-            if not _is_meta_information_sentence:
+            if not _is_meta_information_sentence(sentence_without_leading_spaces):
                 article_sentences.append(sentence_without_leading_spaces)
 
     return article_sentences
@@ -24,7 +24,7 @@ def extract_single_article_from_file(input_file_name):
 
 def _is_meta_information_sentence(sentence):
     last_character = sentence[-1:]
-    return last_character == DOT or last_character == INTERROGATION
+    return last_character != DOT and last_character != INTERROGATION
 
 
 def _remove_all_quote_signs(text):
